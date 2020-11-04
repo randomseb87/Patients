1409be1563990336fecdf28c554a3ea87fca54ef
Sebastian Olsson <olssonsebastian87@gmail.com>
2020-11-04 09:16:05 +0100

    Add initial project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
